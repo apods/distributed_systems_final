55ebec276c75830d17bf426ddc63aeebd3b37d02
Gino <gino-dev@gino-lab.(none)>
2011-12-08 14:32:15 -0500

    Started rate_update method in PowerAlgo, added methods to cluster_manager for getting the execution rate on an arbitrary list of servers and the max for an arbitrary list of servers.

--- lib/power_algo.rb
+++ lib/power_algo.rb
@@ -3,13 +3,17 @@ class PowerAlgo
   attr_writer :server_list, :cluster_manager
   
   def initialize
-    @server_list = nil
+    @server_list = nil  # cluster.rb
     @execution_rate = 0
-    @cluster_manager = nil
+    @cluster_manager = nil # cluster_manager.rb
   end
   
   def rate_update(job_arrival_rate, execution_rate)
-    #implement
+    if job_arrival_rate > execution_rate
+      off_servers = @cluster_manager.get_off
+    end
+    
+    
   end
   
   def server_on(server_id)
